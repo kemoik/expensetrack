7034d9d131f48c82f83fbd2dc2f185d4289cd9e5
kemoik <kemoicaleb17@gmail.com>
2023-07-02 17:27:05 +0300

    updated tcomponents to use Context

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,8 +4,8 @@ export const Transaction = ({transaction}) => {
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
-    <li className="minus">
-          {transaction.text} <span>-Ksh400</span>
+    <li className= {transaction.amount <0 ? 'minus' : 'plus'}>
+          {transaction.text} <span>{sign}Ksh{Math.abs(transaction.amount)}</span>
           <button class="delete-btn">x</button>
         </li>
   )
